a90f699c7e0944db0e39b7e87373f4f0a1286f13
arnold <arnoldkmaina@gmail.com>
2024-07-04 11:34:15 +0300

    python-p4-working-with-apis

--- lib/nyc_api.py
+++ lib/nyc_api.py
@@ -5,10 +5,23 @@ import json
 class GetPrograms:
 
   def get_programs(self):
-    URL = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
+    URL = "https://proxy.dev.socrata.com/socrata/data.cityofnewyork.us/resource/75e9-fg2t.json"
 
     response = requests.get(URL)
     return response.content
+  def program_agencies(self):
+    # we use the JSON library to parse the API response into nicely formatted JSON
+        programs_list = []
+        programs = json.loads(self.get_programs())
+        for program in programs:
+            programs_list.append(program["cbo_name"])
 
-programs = GetPrograms().get_programs()
-print(programs)
+        return programs_list
+
+#programs = GetPrograms().get_programs()
+#print(programs)
+programs = GetPrograms()
+agencies = programs.program_agencies()
+
+for agency in set(agencies):
+    print(agency)
